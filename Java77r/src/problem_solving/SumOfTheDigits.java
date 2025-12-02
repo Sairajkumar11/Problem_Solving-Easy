80020039125f47c1ef37473e44352e1334780928
Sairajkumar11 <sairajkumarsrk11@gmail.com>
2025-12-02 17:16:10 +0530

    Code 1

---
+++ Java77r/src/problem_solving/SumOfTheDigits.java
@@ -0,0 +1,18 @@
+package problem_solving;
+
+public class SumOfTheDigits {
+
+	public static void main(String[] args) {
+		
+		int num=1322,sum=0;
+		while(num!=0) {
+			int n=num%10;
+			sum=sum+n;
+			num/=10;
+		}
+		System.out.println(sum);
+		
+
+	}
+
+}
