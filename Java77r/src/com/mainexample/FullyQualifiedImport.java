e55db001855a4682d71aaa735f35376c6732b07c
Sairajkumar11 <sairajkumarsrk11@gmail.com>
2025-12-15 23:14:18 +0530

    Fully Qualified Import

---
+++ Java77r/src/com/mainexample/FullyQualifiedImport.java
@@ -0,0 +1,11 @@
+package com.mainexample;
+public class FullyQualifiedImport {
+
+	public static void main(String[] args) {
+		
+		com.stringexample.String s=new com.stringexample.String();
+		s.printStr();
+
+	}
+
+}
---
+++ Java77r/src/com/stringexample/String.java
@@ -0,0 +1,9 @@
+package com.stringexample;
+
+public class String {
+	
+	public void printStr() {
+		System.out.println("This from String class");
+	}
+
+}
